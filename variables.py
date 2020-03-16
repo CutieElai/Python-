88b8c20155d165b54dc9bdaa78497d43cc615891
Elisa Busson <60440319+CutieElai@users.noreply.github.com>
2020-03-16 18:40:27 -0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,25 @@
+#Una variable es un nombre simbolico para un valor 
+name = "Elisa"
+#Es como decirle, este string lo estoy guardando dentro de la variable "name"
+print(name)
+#En una variable no solo puedo guardar un string, sino un numero, una lista etc.
+
+#Case Sensitive
+Book = "Demian"
+book = "Lolita"
+x = 3
+
+x, book = 3, "Lolita"
+
+print(x, book)
+print(x)
+print(book)
+
+#Conventiones
+BookName = "Demian" #Skake Case
+bookName = "Demian" #Camel Case
+Book_Name = "Demian" #Pascal case 
+#Esto sirve para que el codigo sea mas legible
+
+PI = 3.14116
+#Escribir todo en mayuscula ayuda a decirle al interprete que nuestro valor no va nunca a cambiar. 
